3b1206f7f0c3872ec7d42bd2b7f5e6a4de469345
agr1002 <44772131+agr1002@users.noreply.github.com>
2021-04-17 17:10:17 +0200

    Instanciar vehículo conductor en la primera posición de salida
    Issue #8

--- game/Assets/Scripts/Entities/RaceController.cs
+++ game/Assets/Scripts/Entities/RaceController.cs
@@ -7,4 +7,19 @@ public class RaceController : MonoBehaviour
     /* Array de transformaciones que definirán los puntos de salida. */
     public Transform[] StartPositions;
 
+    /* Vehículo del juego que conducimos. */
+    public GameObject Driver;
+
+    /* Resto de vehículos de la escena. */
+    public GameObject[] NPCDrivers = new GameObject[7];
+
+    /* Posición en el eje X de los vehículos en su instanciación. */
+    private int PosicionX = 0;
+
+    void Start()
+    {
+        Instantiate(Driver, new Vector3(PosicionX += 4, 0, 0), Quaternion.identity);
+        for (int i = 0; i < NPCDrivers.Length; i++)
+            Instantiate(NPCDrivers[i], new Vector3(PosicionX += 4, 0, 0), Quaternion.identity);
+    }
 }
