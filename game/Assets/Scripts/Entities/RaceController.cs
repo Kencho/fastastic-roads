1decb099cba03ffd8fab4ae3052c0409ac7b616b
agr1002 <44772131+agr1002@users.noreply.github.com>
2021-04-26 12:05:54 +0200

    Insertar diferentes tipos de terreno en la escena de pruebas
    Añadida carretera como microcircuito para hacer distinción del plano hierba (layers "Road" y "Ground" respectivamente). Actualizado script RaceController respecto a la generación de los vehículos según las posiciones indicadas en los objetos de ese mismo tipo.
    
    Issue #11

--- game/Assets/Scripts/Entities/RaceController.cs
+++ game/Assets/Scripts/Entities/RaceController.cs
@@ -14,12 +14,19 @@ public class RaceController : MonoBehaviour
     public GameObject[] NPCDrivers = new GameObject[7];
 
     /* Posición en el eje X de los vehículos en su instanciación. */
-    private int PosicionX = 0;
+    private float PosicionX = 0;
 
     void Start()
     {
-        Instantiate(Driver, new Vector3(PosicionX += 4, 0, 0), Quaternion.identity);
+        if(StartPositions.Length != 0 && StartPositions[0] != null)
+            Instantiate(Driver, new Vector3(PosicionX = StartPositions[0].position.x, StartPositions[0].position.y, StartPositions[0].position.z), Quaternion.identity);
+        else
+            Instantiate(Driver, new Vector3(PosicionX += 5, 0, 0), Quaternion.identity);        
+
         for (int i = 0; i < NPCDrivers.Length; i++)
-            Instantiate(NPCDrivers[i], new Vector3(PosicionX += 4, 0, 0), Quaternion.identity);
+            if (StartPositions[i] != null)
+                Instantiate(NPCDrivers[i], new Vector3(PosicionX = StartPositions[i+1].position.x, StartPositions[i+1].position.y, StartPositions[i+1].position.z), Quaternion.identity);
+            else
+                Instantiate(NPCDrivers[i], new Vector3(PosicionX += 5, 0, 0), Quaternion.identity);
     }
 }
