fc9ac3508f76c091323978ba4c4647f5245ed690
agr1002 <44772131+agr1002@users.noreply.github.com>
2021-06-01 20:01:02 +0200

    Añadir a RaceController un contador de vueltas por corredor
    Issue #42

--- game/Assets/Scripts/OtherControllers/RaceController.cs
+++ game/Assets/Scripts/OtherControllers/RaceController.cs
@@ -18,10 +18,10 @@ public class RaceController : MonoBehaviour
     /* Posición en el eje X de los vehículos en su instanciación. */
     private float PosicionX;
 
-    /* */
+    /* Posición de inicio de partida del vehículo. */
     public static Vector3 initPosition;
 
-    /* */
+    /* Rotación de inicio de partida del vehículo. */
     public static Quaternion initRotation;
 
     /* Vehículo que conducimos en la escena para su reaparecimiento. */
@@ -36,41 +36,60 @@ public class RaceController : MonoBehaviour
     /* Tiempo de espera al finalizar la carrera para el cambio de escena. */
     private float WaitTime;
 
-    /*  */
+    /* Checkpoint correspondiente a la línea de meta. */
     public Checkpoint FinishLine;
 
-    public int raceId = 0;
+    /* Identificador de corredor. */
+    private int raceId = 0;
+
+    /* Contiene el número de vueltas dada por cada corredor. */
+    private int[] racerLaps;
+
+    /* Número de vehículos total en la partida. */
+    private int vehicleNumber = 1;
 
     void Start()
     {
         GameObject vehicle;
         WaitTime = 0;
+        
+        for (int i = 0; i < NPCDrivers.Length; i++) if (NPCDrivers[i] != null) vehicleNumber++;
+        racerLaps = new int[vehicleNumber];
 
         if (StartPositions.Length != 0 && StartPositions[0] != null)
             vehicle = Instantiate(Driver, new Vector3(PosicionX = StartPositions[0].position.x, StartPositions[0].position.y, StartPositions[0].position.z), Quaternion.Euler(StartPositions[0].eulerAngles.x, StartPositions[0].eulerAngles.y, StartPositions[0].eulerAngles.z));
         else
             vehicle = Instantiate(Driver, new Vector3(PosicionX += 5, 0, 0), Quaternion.identity);
-
+        
         vehicle.SendMessage("SetLastCheckpoint", FinishLine);
         vehicle.SendMessage("SetRaceId", raceId);
-        Debug.Log("El id de vehículo conductor es: " + raceId);
+        racerLaps[raceId] = 1;
         raceId++;
+
         initPosition = vehicle.transform.position;
         initRotation = vehicle.transform.rotation;
 
+        vehicleNumber++;
+        Debug.Log("El tamaño de NPCDrivers es de: " + NPCDrivers.Length);
         for (int i = 0; i < NPCDrivers.Length; i++)
         {
-            if (StartPositions[i] != null)
-                vehicle = Instantiate(NPCDrivers[i], new Vector3(PosicionX = StartPositions[i + 1].position.x, StartPositions[i + 1].position.y, StartPositions[i + 1].position.z), Quaternion.Euler(StartPositions[i + 1].eulerAngles.x, StartPositions[i + 1].eulerAngles.y, StartPositions[i + 1].eulerAngles.z));
-            else
-                vehicle = Instantiate(NPCDrivers[i], new Vector3(PosicionX += 5, 0, 0), Quaternion.identity);
-            
-            vehicle.SendMessage("SetLastCheckpoint", FinishLine);
-            vehicle.SendMessage("SetRaceId", raceId);
-            Debug.Log("El id del vehículo NPC " + i + " es: " + raceId);
-            raceId++;
-            
+            if (NPCDrivers[i] != null)
+            {
+                if (StartPositions[i] != null)
+                    vehicle = Instantiate(NPCDrivers[i], new Vector3(PosicionX = StartPositions[i + 1].position.x, StartPositions[i + 1].position.y, StartPositions[i + 1].position.z), Quaternion.Euler(StartPositions[i + 1].eulerAngles.x, StartPositions[i + 1].eulerAngles.y, StartPositions[i + 1].eulerAngles.z));
+                else
+                    vehicle = Instantiate(NPCDrivers[i], new Vector3(PosicionX += 5, 0, 0), Quaternion.identity);
+
+                vehicle.SendMessage("SetLastCheckpoint", FinishLine);
+                vehicle.SendMessage("SetRaceId", raceId);
+                Debug.Log("El id del vehículo NPC " + i + " es: " + raceId);
+                racerLaps[raceId] = 1;
+                raceId++;
+                vehicleNumber++;
+            }
         }
+
+        Debug.Log("RacerLaps tiene un tamaño de: " + vehicleNumber);
     }
 
     void Update()
